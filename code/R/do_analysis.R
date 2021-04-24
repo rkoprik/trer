33addc5e3b0b77cc90011ba3d53789fee273c443
rkoprik <rebecca.staniscia.koprik@hu-berlin.de>
2021-04-24 18:04:58 +0200

    myproject

--- code/R/do_analysis.R
+++ code/R/do_analysis.R
@@ -38,6 +38,48 @@ tab_regression <-  prepare_regression_table(
   format = "latex"
 )
 
+# My contribution
+
+# Table - life expect by income level
+  tab_expand <- aggregate(smp$life_expectancy, 
+              by=list(Income=smp$income_level), 
+              FUN=mean)
+  tab_expand <- tab_expand %>% 
+    # desc orders from largest to smallest
+    arrange(desc(x))
+  names(tab_expand)[names(tab_expand) == "x"] <- "Life Expectancy"
+  names(tab_expand)[names(tab_expand) == "Income"] <- "World Bank Income Group"
+  
+# By income level Average - evolution of life_expectancy
+  # Avg per income level
+  tab_group_avg <- aggregate(smp$life_expectancy, 
+                          by=list(Year=smp$year, Income=smp$income_level), 
+                          FUN=mean)
+  # plot
+  fig_scatter_level <- ggplot(data = tab_group_avg, aes(x=Year, y=x)) + 
+    geom_line(aes(colour=Income)) +
+    labs(
+      color = "World Bank Income Level", 
+      x = "Year",
+      y = "Life expectancy in years"
+    ) +
+    theme_minimal()
+
+# General Average - evolution of life_expectancy
+  # average calc
+  tab_gen_avg <- aggregate(life_expectancy ~ year, data=smp, mean)
+  # evolution of life_expectancy
+  fig_scatter_avg <- ggplot(
+    tab_gen_avg, aes(x = year, y = life_expectancy)
+  ) +
+    geom_line() +
+    labs(
+      color = "World Bank Region",
+      x = "Year",
+      y = "Life expectancy in years"
+    ) +
+    theme_minimal()
+  
 
 save(
   list = c(ls(pattern = "fig_*"), ls(pattern = "tab_*")),
